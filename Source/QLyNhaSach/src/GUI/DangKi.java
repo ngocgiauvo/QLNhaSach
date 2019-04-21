3d793d6554357a6b5d251b24f164b7ead62c25e7
OanhNguyen101096 <oanhnguyen1740244@gmail.com>
2019-04-21 19:11:12 +0700

    Update trang đăng kí

--- Source/QLyNhaSach/src/GUI/DangKi.java
+++ Source/QLyNhaSach/src/GUI/DangKi.java
@@ -12,6 +12,7 @@ import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JTextField;
+import java.awt.Color;
 
 public class DangKi extends JFrame {
 
@@ -19,6 +20,11 @@ public class DangKi extends JFrame {
 	private JTextField txtTenDangNhap;
 	private JTextField txtMatKhau;
 	private JTextField txtNhapLaiMK;
+	private JTextField txtHoten;
+	private JTextField txtCmnd;
+	private JTextField txtDiaChi;
+	private JTextField txtSDT;
+	private JTextField txtEmail;
 
 	/**
 	 * Launch the application.
@@ -41,50 +47,98 @@ public class DangKi extends JFrame {
 	 */
 	public DangKi() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 404, 427);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
 		JLabel lblngK = new JLabel("\u0110\u0102NG K\u00DD");
+		lblngK.setForeground(Color.BLUE);
 		lblngK.setFont(new Font("Times New Roman", Font.BOLD, 24));
-		lblngK.setBounds(142, 11, 139, 26);
+		lblngK.setBounds(143, 29, 139, 26);
 		contentPane.add(lblngK);
 		
 		JLabel lblUsername = new JLabel("T\u00EAn \u0111\u0103ng nh\u1EADp");
-		lblUsername.setBounds(33, 66, 78, 14);
+		lblUsername.setBounds(33, 78, 78, 26);
 		contentPane.add(lblUsername);
 		
 		JLabel lblMtKhu = new JLabel("M\u1EADt Kh\u1EA9u");
-		lblMtKhu.setBounds(33, 91, 46, 14);
+		lblMtKhu.setBounds(33, 118, 46, 14);
 		contentPane.add(lblMtKhu);
 		
 		JLabel lblNhpLiMt = new JLabel("Nh\u1EADp l\u1EA1i m\u1EADt kh\u1EA9u");
-		lblNhpLiMt.setBounds(33, 116, 112, 14);
+		lblNhpLiMt.setBounds(33, 143, 112, 14);
 		contentPane.add(lblNhpLiMt);
 		
 		JButton btnngK = new JButton("\u0110\u0103ng K\u00FD");
+		btnngK.setBackground(Color.GREEN);
+		btnngK.setFont(new Font("Tahoma", Font.BOLD, 12));
 		btnngK.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 			}
 		});
-		btnngK.setBounds(223, 171, 89, 23);
+		btnngK.setBounds(223, 354, 89, 23);
 		contentPane.add(btnngK);
 		
 		txtTenDangNhap = new JTextField();
-		txtTenDangNhap.setBounds(143, 63, 169, 20);
+		txtTenDangNhap.setBounds(142, 81, 169, 20);
 		contentPane.add(txtTenDangNhap);
 		txtTenDangNhap.setColumns(10);
 		
 		txtMatKhau = new JTextField();
-		txtMatKhau.setBounds(142, 88, 170, 20);
+		txtMatKhau.setBounds(142, 112, 170, 20);
 		contentPane.add(txtMatKhau);
 		txtMatKhau.setColumns(10);
 		
 		txtNhapLaiMK = new JTextField();
-		txtNhapLaiMK.setBounds(142, 113, 170, 20);
+		txtNhapLaiMK.setBounds(142, 140, 170, 20);
 		contentPane.add(txtNhapLaiMK);
 		txtNhapLaiMK.setColumns(10);
+		
+		JLabel lblNewLabel = new JLabel("H\u1ECD T\u00EAn");
+		lblNewLabel.setBounds(33, 174, 69, 14);
+		contentPane.add(lblNewLabel);
+		
+		txtHoten = new JTextField();
+		txtHoten.setColumns(10);
+		txtHoten.setBounds(142, 171, 170, 20);
+		contentPane.add(txtHoten);
+		
+		JLabel lblCmnd = new JLabel("CMND");
+		lblCmnd.setBounds(33, 205, 69, 14);
+		contentPane.add(lblCmnd);
+		
+		txtCmnd = new JTextField();
+		txtCmnd.setColumns(10);
+		txtCmnd.setBounds(142, 202, 170, 20);
+		contentPane.add(txtCmnd);
+		
+		JLabel lblaCh = new JLabel("\u0110\u1ECBa Ch\u1EC9");
+		lblaCh.setBounds(33, 236, 69, 14);
+		contentPane.add(lblaCh);
+		
+		txtDiaChi = new JTextField();
+		txtDiaChi.setColumns(10);
+		txtDiaChi.setBounds(142, 233, 170, 20);
+		contentPane.add(txtDiaChi);
+		
+		JLabel lblSt = new JLabel("S\u0110T");
+		lblSt.setBounds(33, 267, 69, 14);
+		contentPane.add(lblSt);
+		
+		txtSDT = new JTextField();
+		txtSDT.setColumns(10);
+		txtSDT.setBounds(142, 264, 170, 20);
+		contentPane.add(txtSDT);
+		
+		JLabel lblEmail = new JLabel("Email");
+		lblEmail.setBounds(33, 298, 69, 14);
+		contentPane.add(lblEmail);
+		
+		txtEmail = new JTextField();
+		txtEmail.setColumns(10);
+		txtEmail.setBounds(142, 295, 170, 20);
+		contentPane.add(txtEmail);
 	}
 }
