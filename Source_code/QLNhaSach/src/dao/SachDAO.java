33d52521a302d3321859170f3112db80393700a7
ngocgiauvo <ngocgiau18tqdn@gmail.com>
2019-05-03 23:40:51 +0700

    update "Danh sách sách" - add search function (done 100%)

--- Source_code/QLNhaSach/src/dao/SachDAO.java
+++ Source_code/QLNhaSach/src/dao/SachDAO.java
@@ -2,11 +2,13 @@ package dao;
 
 import java.util.ArrayList;
 
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 import entities.Sach;
+import entities.Taikhoan;
 
 public class SachDAO {
 	public SachDAO() {
@@ -30,5 +32,38 @@ public class SachDAO {
 			session.close();
 		}
 		return list;
+	}
+	
+	public ArrayList<Sach> timKiemSach(String ten, String tacgia, int loai) {
+		Session session = sessionFactory.openSession();
+		ArrayList<Sach> list = null;
+		
+		try {
+			session.beginTransaction();
+			
+			String hql = "FROM Sach WHERE tenSach LIKE :ten AND tacGia LIKE :tacgia";
+			if(loai > 0) {
+				hql += " AND theLoai = :loai";
+			}
+			
+			Query query = session.createQuery(hql);
+			query.setParameter("ten", "%" + ten + "%");
+			query.setParameter("tacgia", "%" + tacgia + "%");
+			
+			if(loai > 0) {
+				query.setInteger("loai", loai);
+			}
+			
+			list = (ArrayList<Sach>) query.list();
+			
+			return list;
+		} catch (RuntimeException e) {
+			session.getTransaction().rollback();
+			e.printStackTrace();
+		} finally {
+			session.flush();
+			session.close();
+		}
+		return null;
 	}
 }
--- Source_code/QLNhaSach/src/gui/DSSach.java
+++ Source_code/QLNhaSach/src/gui/DSSach.java
@@ -16,19 +16,27 @@ import java.awt.Font;
 import java.util.ArrayList;
 
 import javax.swing.JTable;
+import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JTextField;
+import javax.swing.JComboBox;
 
 public class DSSach extends JPanel {
 	private JTable tblDSSach;
 	private static ArrayList<Theloai> theloai;
+	private JTextField txtTenSach;
+	private JTextField txtTacGia;
 
 	/**
 	 * Create the panel.
 	 */
 	public DSSach() {
 		setLayout(null);
+		
+		TheLoaiDAO tlDAO = new TheLoaiDAO();
+		theloai = tlDAO.layTheLoai();
 		
 		JPanel panelHead = new JPanel();
 		panelHead.setBounds(10, 11, 865, 40);
@@ -41,11 +49,77 @@ public class DSSach extends JPanel {
 		lblDanhSchSch.setHorizontalAlignment(SwingConstants.CENTER);
 		panelHead.add(lblDanhSchSch);
 		
-		JButton btnLamMoi = new JButton("Làm mới");
-		btnLamMoi.addActionListener(new ActionListener() {
+		JPanel panelMain = new JPanel();
+		panelMain.setBounds(10, 113, 865, 416);
+		add(panelMain);
+		panelMain.setLayout(new BorderLayout(0, 0));
+		
+		tblDSSach = new JTable();
+		panelMain.add(new JScrollPane(tblDSSach), BorderLayout.CENTER);
+		
+		ArrayList<Sach> ds = layDSSach();
+		DefaultTableModel model = setDataForTable(ds);
+		tblDSSach.setModel(model);
+		
+		JPanel panelSearch = new JPanel();
+		panelSearch.setBounds(10, 62, 865, 40);
+		add(panelSearch);
+		panelSearch.setLayout(null);
+		
+		JLabel lblTnSch = new JLabel("Tên sách:");
+		lblTnSch.setBounds(10, 11, 58, 14);
+		panelSearch.add(lblTnSch);
+		
+		txtTenSach = new JTextField();
+		txtTenSach.setBounds(78, 8, 211, 20);
+		panelSearch.add(txtTenSach);
+		txtTenSach.setColumns(10);
+		
+		JLabel lblThLoi = new JLabel("Thể loại:");
+		lblThLoi.setBounds(313, 11, 58, 14);
+		panelSearch.add(lblThLoi);
+		
+		JComboBox cbTheLoai = new JComboBox();
+		cbTheLoai.setBounds(381, 8, 121, 20);
+		
+		DefaultComboBoxModel cbModel = setDataForCombobox();
+		cbTheLoai.setModel(cbModel);
+		cbTheLoai.setSelectedItem(null);
+		
+		panelSearch.add(cbTheLoai);
+		
+		JLabel lblTcGi = new JLabel("Tác giả:");
+		lblTcGi.setBounds(522, 11, 47, 14);
+		panelSearch.add(lblTcGi);
+		
+		txtTacGia = new JTextField();
+		txtTacGia.setBounds(579, 8, 154, 20);
+		panelSearch.add(txtTacGia);
+		txtTacGia.setColumns(10);
+		
+		JButton btnTimSach = new JButton("Tìm");
+		btnTimSach.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				ArrayList<Sach> ds = layDSSach();
-				DefaultTableModel model = setDataForTable(ds);
+				String tensach = txtTenSach.getText();
+				String tacgia = txtTacGia.getText();
+				int loaiId = 0;
+				
+				if(cbTheLoai.getSelectedItem() != null) {
+					String loai = cbTheLoai.getSelectedItem().toString();
+					for(int j = 0; j < theloai.size(); j++) {
+						String temp = theloai.get(j).getTenTheLoai();
+						
+						if(loai.equals(temp)) {
+							loaiId = theloai.get(j).getId();
+						}
+					}
+				}
+				
+				//tim sach theo 3 tieu chi
+				SachDAO sDAO = new SachDAO();
+				ArrayList<Sach> dsSach = sDAO.timKiemSach(tensach, tacgia, loaiId);
+				
+				DefaultTableModel model = setDataForTable(dsSach);
 				tblDSSach.setModel(model);
 				
 				tblDSSach.getColumnModel().getColumn(0).setMaxWidth(40);
@@ -55,23 +129,8 @@ public class DSSach extends JPanel {
 				tblDSSach.getColumnModel().getColumn(4).setMaxWidth(95);
 			}
 		});
-		btnLamMoi.setBounds(776, 11, 89, 23);
-		panelHead.add(btnLamMoi);
-		
-		JPanel panelMain = new JPanel();
-		panelMain.setBounds(10, 62, 865, 467);
-		add(panelMain);
-		panelMain.setLayout(new BorderLayout(0, 0));
-		
-		tblDSSach = new JTable();
-		panelMain.add(new JScrollPane(tblDSSach), BorderLayout.CENTER);
-		
-		TheLoaiDAO tlDAO = new TheLoaiDAO();
-		theloai = tlDAO.layTheLoai();
-		
-		ArrayList<Sach> ds = layDSSach();
-		DefaultTableModel model = setDataForTable(ds);
-		tblDSSach.setModel(model);
+		btnTimSach.setBounds(766, 7, 89, 23);
+		panelSearch.add(btnTimSach);
 		
 		tblDSSach.getColumnModel().getColumn(0).setMaxWidth(40);
 		tblDSSach.getColumnModel().getColumn(1).setMaxWidth(350);
@@ -113,5 +172,16 @@ public class DSSach extends JPanel {
 			}
 		}
 		return null;
+	}
+	
+	public static DefaultComboBoxModel setDataForCombobox() {
+		String[] data = new String[theloai.size()];
+		for(int i = 0; i < theloai.size(); i++) {
+			data[i] = theloai.get(i).getTenTheLoai();
+		}
+		
+		DefaultComboBoxModel model = new DefaultComboBoxModel(data);
+		
+		return model;
 	}
 }
