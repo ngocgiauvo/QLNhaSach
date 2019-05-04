38a4a500931912ff931961a50556b4856d0bc874
ngocgiauvo <ngocgiau18tqdn@gmail.com>
2019-05-04 19:38:35 +0700

    update UI PhieuNhapSach (add row, del row...)

--- Source_code/QLNhaSach/src/gui/PhieuNhapSach.java
+++ Source_code/QLNhaSach/src/gui/PhieuNhapSach.java
@@ -19,6 +19,8 @@ import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.Dimension;
+import java.awt.Font;
+import javax.swing.SwingConstants;
 
 public class PhieuNhapSach extends JPanel {
 	private JTable tblNhapsach;
@@ -30,16 +32,22 @@ public class PhieuNhapSach extends JPanel {
 		setLayout(null);
 		
 		JPanel panelHead = new JPanel();
-		panelHead.setBounds(10, 11, 680, 39);
+		panelHead.setBounds(10, 11, 865, 87);
 		panelHead.setLayout(null);
 		add(panelHead);
 		
 		JLabel lblNgyNhp = new JLabel("Ngày nhập:");
-		lblNgyNhp.setBounds(59, 11, 81, 17);
+		lblNgyNhp.setBounds(246, 59, 81, 17);
 		panelHead.add(lblNgyNhp);
 		
+		JLabel lblPhiuNhpSch = new JLabel("PHIẾU NHẬP SÁCH");
+		lblPhiuNhpSch.setHorizontalAlignment(SwingConstants.CENTER);
+		lblPhiuNhpSch.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblPhiuNhpSch.setBounds(98, 8, 673, 40);
+		panelHead.add(lblPhiuNhpSch);
+		
 		JPanel panelMain = new JPanel();
-		panelMain.setBounds(10, 61, 535, 253);
+		panelMain.setBounds(10, 109, 725, 420);
 		add(panelMain);
 		panelMain.setLayout(new BorderLayout(0, 0));
 		
@@ -49,25 +57,48 @@ public class PhieuNhapSach extends JPanel {
 		DefaultTableModel model = setDataForTable(null);
 		tblNhapsach.setModel(model);
 		
+		tblNhapsach.getColumnModel().getColumn(0).setMaxWidth(30);
+		tblNhapsach.getColumnModel().getColumn(1).setMaxWidth(320);
+		tblNhapsach.getColumnModel().getColumn(2).setMaxWidth(150);
+		tblNhapsach.getColumnModel().getColumn(3).setMaxWidth(150);
+		tblNhapsach.getColumnModel().getColumn(4).setMaxWidth(75);
+		
 		JPanel panelBtn = new JPanel();
-		panelBtn.setBounds(560, 62, 130, 252);
+		panelBtn.setBounds(745, 109, 130, 420);
 		add(panelBtn);
 		panelBtn.setLayout(null);
 		
 		JButton btnThem = new JButton("Thêm");
+		btnThem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				DefaultTableModel curModel = (DefaultTableModel) tblNhapsach.getModel();
+				
+				Object rowData[] = new Object[1];
+				curModel.addRow(rowData);
+				
+				tblNhapsach.setModel(curModel);
+			}
+		});
 		btnThem.setBounds(10, 11, 110, 28);
 		panelBtn.add(btnThem);
 		
-		JButton btnSua = new JButton("Sửa");
-		btnSua.setBounds(10, 50, 110, 28);
-		panelBtn.add(btnSua);
-		
 		JButton btnXoa = new JButton("Xóa");
-		btnXoa.setBounds(10, 89, 110, 28);
+		btnXoa.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				DefaultTableModel curModel = (DefaultTableModel) tblNhapsach.getModel();
+				
+				int select = tblNhapsach.getSelectedRow();
+				
+				curModel.removeRow(select);
+				
+				tblNhapsach.setModel(curModel);
+			}
+		});
+		btnXoa.setBounds(10, 50, 110, 28);
 		panelBtn.add(btnXoa);
 		
 		JButton btnNhapSach = new JButton("Nhập sách");
-		btnNhapSach.setBounds(10, 128, 110, 28);
+		btnNhapSach.setBounds(10, 89, 110, 28);
 		panelBtn.add(btnNhapSach);
 	}
 	
